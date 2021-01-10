7ac0065017dadc3b1c23f5512776ea2eeb0d5c0b
tilak9835 <tilak.echarge@gmail.com>
2021-01-10 18:01:35 +0530

    Updates

---
+++ precache-manifest.817b88d5011a6874dc97a1262fd6a0c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29f88c5bc01b454db853d31d93c97d9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "822746fd14a9d5953254",
+    "url": "/static/css/main.cea3268d.chunk.css"
+  },
+  {
+    "revision": "ea1cb07332a0a8157d78",
+    "url": "/static/js/2.0bdea6f6.chunk.js"
+  },
+  {
+    "revision": "a9dd647aec81fffade4fa7e6d693dad7",
+    "url": "/static/js/2.0bdea6f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "822746fd14a9d5953254",
+    "url": "/static/js/main.df0cf149.chunk.js"
+  },
+  {
+    "revision": "a899309b89ebd42b7111",
+    "url": "/static/js/runtime-main.89df0827.js"
+  }
+]);
